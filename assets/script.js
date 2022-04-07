03e8f7900ce58750ace2ceb7f7b92aa3310548e1
Vilson Apostolovski II <vilsonapo@gmail.com>
2022-04-07 12:26:18 -0400

    Save button saves to local storage and reloads on refresh

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,23 @@
+// Adding current day to top of page
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
 
+//Declaring save buttons
+const saveButton = $(".saveBtn")
+saveButton.on("click", function(event){
+    event.preventDefault();
+    var hour = $(this).siblings(".hour").text();
+    var items = $(this).siblings(".items").val();
+    localStorage.setItem(hour,items);
+});
+
+function getSavedItems() {
+    $(".hour").each(function(){
+        var currHour = $(this).text();
+        var currItem = localStorage.getItem(currHour);
+        if (currItem !== null){
+            $(this).siblings(".items").val(currItem);
+        }
+    });
+}
+
+getSavedItems();
